3ef95b332b8ea43bf6215de5ea7b44d0860f7466
Davide Marcandelli <davide.marcandelli@gmail.com>
2020-12-03 15:58:02 +0100

    first push

---
+++ src/main/java/com/finalproject/treebackendroom1/TreeBackendRoom1Application.java
@@ -0,0 +1,13 @@
+package com.finalproject.treebackendroom1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreeBackendRoom1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreeBackendRoom1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/finalproject/treebackendroom1/TreeBackendRoom1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalproject.treebackendroom1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreeBackendRoom1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
